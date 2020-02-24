fb59282b1191efc879ee81b79408eb9acd7e3796
Yash Garudkar <yashgarudkar@gmail.com>
2020-02-25 05:12:19 +0530

    treks done

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,39 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
 //welcome page
-router.get('/',(req, res, next)=>{
+router.get('/', (req, res, next) => {
     res.render('welcome');
 })
 
 //customer dashboard
-router.get('/main', ensureAuthenticated , (req, res)=>{
+router.get('/main', ensureAuthenticated, (req, res) => {
     res.render('main');
 });
 
 //packages
-router.get('/packs', ensureAuthenticated , (req, res, next)=>{
+router.get('/packs', ensureAuthenticated, (req, res, next) => {
     res.render('packages')
 });
 
+router.get('/packs:id', ensureAuthenticated, (req, res, next) => {
+    
+});
+
+
 //treks and waterfalls
-router.get('/tandw', ensureAuthenticated , (req, res)=>{
+router.get('/tandw', ensureAuthenticated, (req, res) => {
     res.render('tandw');
 });
 
 //temples
-router.get('/temples', ensureAuthenticated , (req, res)=>{
+router.get('/temples', ensureAuthenticated, (req, res) => {
     res.render('temples');
 });
 
 //admindashboard
-router.get('/admindashb', ensureAuthenticated , (req, res)=>{
+router.get('/admindashb', ensureAuthenticated, (req, res) => {
     res.render('admin');
 });
 
