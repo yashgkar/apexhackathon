ff9e241df169ee17c2162de4c7b32404163395b0
Yash Garudkar <yashgarudkar@gmail.com>
2020-02-25 06:44:09 +0530

    got submit button inside form

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 app.use('/packs', require('./routes/packages'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
